20a152b9de57e0ebbf7b47d8467bd0d48adafca7
SolusN <solus_94@live.se>
2025-02-20 19:03:56 +0100

    Added ticks

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
+	clock = pygame.time.Clock
+	dt = 0
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -18,7 +21,7 @@ def main():
 		pygame.Surface.fill(screen,(0,0,0))
 
 
-
+		dt = clock.tick(60) / 1000
 		pygame.display.flip()
 if __name__ == "__main__":
 	main()
