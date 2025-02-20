4a1e0847c3b1dc841314616ba5b8ef2eb1ab921f
SolusN <solus_94@live.se>
2025-02-20 18:57:53 +0100

    Found the error and probably fixed it

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from constants import *
 
 def main():
 	pygame.init()
-	pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
 	while True:
-		pygame.Surface.fill((0,0,0))
+		pygame.Surface.fill(screen,(0,0,0))
 
 
 		pygame.display.flip()
