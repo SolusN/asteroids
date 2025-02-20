0fd005450a3c15a3cb8a2af854013a2da788fbc6
SolusN <solus_94@live.se>
2025-02-20 18:59:29 +0100

    added quit

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
 	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
 		pygame.Surface.fill(screen,(0,0,0))
+
 
 
 		pygame.display.flip()
