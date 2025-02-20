edc8e79b70ff461adfa057333648bc3b9662df5a
SolusN <solus_94@live.se>
2025-02-20 18:50:15 +0100

    "did all steps up to 6 on asteroids"

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from constants import *
 
 def main():
 	pygame.init()
-	display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
 	while True:
-		screen.fill(color=black)
+		pygame.screen.fill(color=black)
 
 
-		display.flip()
+		pygame.display.flip()
 if __name__ == "__main__":
 	main()
