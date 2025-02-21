347649b015b38f6cca1f3662564af29610b4dab6
SolusN <solus_94@live.se>
2025-02-21 18:08:17 +0100

    fixed wrong format on functioncall

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill(0,0,0)
+		screen.fill((0,0,0))
 		print(clock)
 		print(type(clock))
 		
