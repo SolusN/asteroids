745a8b32ea1319c9f4f202b59022ef80ad1f5cfb
SolusN <solus_94@live.se>
2025-02-23 15:51:38 +0100

    added everything from c2:l5

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
 	clock = pygame.time.Clock()
 	dt = 0
 	try:
@@ -21,11 +24,12 @@ def main():
 					return
 			screen.fill((0,0,0))
 			
+			player.draw(screen)
 			pygame.display.update()
 	except KeyboardInterrupt:
 		print("Game loop interrupted. Quitting...")
 	finally:
 		pygame.quit()
-		
+
 if __name__ == "__main__":
 	main()
