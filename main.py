0b3a894e742d73e52c2565aa7b29aadccc01aa22
SolusN <solus_94@live.se>
2025-02-23 16:51:07 +0100

    missed 'dt', added to call

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main():
 				if event.type == pygame.QUIT:
 					return
 			screen.fill((0,0,0))
-			my_player.update()
+			my_player.update(dt)
 			my_player.draw(screen)
 			pygame.display.update()
 	except KeyboardInterrupt:
