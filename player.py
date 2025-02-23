cf4c42cb20f33c39c5677dcff5a958a3139e70cf
SolusN <solus_94@live.se>
2025-02-23 15:55:10 +0100

    fix 4

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import constants
 import circleshape
 
-class Player(CircleShape):
+class Player(circleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
