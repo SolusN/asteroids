c34d626014d2fb0bef929a1b7de80d67ad17791b
SolusN <solus_94@live.se>
2025-02-25 18:42:34 +0100

    Added controls to Shoot

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 import constants
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -26,6 +27,10 @@ class Player(CircleShape):
         forward = pygame.Vector2(0,1).rotate(self.rotation)
         self.position += forward * constants.PLAYER_MOVE_SPEED * dt
 
+    def shoot(self, dt):
+        shot = Shot(self.x, self.y, constants.SHOT_RADIUS)
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * constants.PLAYER_SHOOT_SPEED
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
         
@@ -38,4 +43,8 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+        if keys[pygame.K_SPACE]:
+            self.shoot(dt)
+
         
